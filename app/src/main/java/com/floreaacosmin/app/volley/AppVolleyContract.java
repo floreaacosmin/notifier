f6268b8b634ba8798572f60baa6d6285962ec4ff
Cosmin Florea <floreaacosmin@yahoo.com>
2017-11-04 23:25:47 +0200

    04112017

--- app/src/main/java/com/floreaacosmin/app/volley/AppVolleyContract.java
+++ app/src/main/java/com/floreaacosmin/app/volley/AppVolleyContract.java
@@ -6,8 +6,12 @@ class AppVolleyContract {
 
     // URL to fetch content from during a sync
     // public static final String API_URL = "http://10.0.177.253:8080/notifications/all";
-    public static final String API_URL = "http://192.168.10.4:8080/notifications/all";
+    private static final String API_URL_LOCAL = "http://192.168.10.4:8080";
+    private static final String API_URL_OPENSHIFT = "http://notifier-rest-notifier-db.7e14.starter-us-west-2.openshiftapps.com";
+    private static final String API_SUFFIX = "/notifications/all";
 
+    public static final String API_URL = API_URL_OPENSHIFT + API_SUFFIX;
+    
 	/* When working with JSON data in Android, a JSONArray is used to parse JSON which starts with
 	* the array brackets. Arrays in JSON are used to organize a collection of related items. On
 	* the other hand, JSONObject is used when dealing with a JSON that begins with curly braces.
