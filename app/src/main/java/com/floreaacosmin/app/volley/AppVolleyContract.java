62e3fb9422cfd234096d1f0f2fbbe042d11b7a83
floreaacosmin <floreaacosmin@yahoo.com>
2017-11-13 23:57:18 +0200

    13112017

--- app/src/main/java/com/floreaacosmin/app/volley/AppVolleyContract.java
+++ app/src/main/java/com/floreaacosmin/app/volley/AppVolleyContract.java
@@ -5,13 +5,14 @@ class AppVolleyContract {
     }
 
     // URL to fetch content from during a sync
-    // public static final String API_URL = "http://10.0.177.253:8080/notifications/all";
     private static final String API_URL_LOCAL_1 = "http://192.168.10.4:8080";
     private static final String API_URL_LOCAL_2 = "http://192.168.10.12:8080";
     private static final String API_URL_OPENSHIFT = "http://notifier-rest-notifier-db.7e14.starter-us-west-2.openshiftapps.com";
     private static final String API_SUFFIX = "/notifications/all";
 
-    public static final String API_URL = API_URL_OPENSHIFT + API_SUFFIX;
+    private static final String ACTIVE_API_URL = API_URL_OPENSHIFT;
+
+    public static final String API_URL = ACTIVE_API_URL + API_SUFFIX;
     
 	/* When working with JSON data in Android, a JSONArray is used to parse JSON which starts with
 	* the array brackets. Arrays in JSON are used to organize a collection of related items. On
